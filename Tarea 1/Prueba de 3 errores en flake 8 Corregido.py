4b49ef1159b2faab5c002e99ae328659ca6017a3
dani <danielacamposacuna2105@gmail.com>
2024-02-16 21:50:12 -0600

    Errorres corregidos

---
+++ Tarea 1/Prueba de 3 errores en flake 8 Corregido.py
@@ -0,0 +1,10 @@
+
+# Corrección de error
+def suma_numeros(a, b):
+    resultado = a + b
+    return resultado  # Corrección de error
+
+numero1 = 10
+numero2 = 20
+
+print("La suma de", numero1, "y", numero2, "es:", suma_numeros(numero1, numero2))  # Corrección de error
